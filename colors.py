976b6c7480e42117921155eff092e39d47576622
malevasquez <male.vasquez@yahoo.com.ar>
2022-08-28 15:51:35 -0300

    added proportions to mixing + functions

--- colors.py
+++ colors.py
@@ -1,49 +1,62 @@
 from colorutils import Color
+from numpy import random
 import math
 
-colors = []
+#calculates the euclidean distance between the rgb spaces of two colors
+def distance(r1, g1, b1, r2, g2, b2):
+    return math.sqrt((math.pow(r2-r1,2) + math.pow(g2-g1,2) + math.pow(b2-b1,2)))
 
-size = int(input("Enter palette size: "))
-exp_r, exp_g, exp_b = map(int, input("Enter comma-separated rgb values of expected color: ").split(','))
-expected = Color((exp_r, exp_g, exp_b))
 
-count = 0
+def mix(r1, g1, b1, r2, g2, b2, p):
+    aux_red = p*math.pow(255-r1,2) + (1-p)*math.pow(255-r2,2)
+    aux_green = p*math.pow(255-g1,2) + (1-p)*math.pow(255-(1-p)*g2,2)
+    aux_blue = p*math.pow(255-p*b1,2) + (1-p)*math.pow(255-(1-p)*b2,2)
 
-while count < size:
-    r, g, b = map(int, input("Enter comma-separated rgb values: ").split(','))
-    colors.append(Color((r,g,b)))
-    count += 1
+    red = round(255-math.sqrt(aux_red)/2)
+    green = round(255-math.sqrt(aux_green)/2)
+    blue = round(255-math.sqrt(aux_blue)/2)
 
-##c1 = Color((153,102,255))
-##c2 = Color((255,255,102))
+    return Color((red,green,blue))
 
-red = 0
-green = 0
-blue = 0
+def fitness(r1, g1, b1, r2, g2, b2):
+    return 1/distance
+
+c1 = Color((153,102,255))
+c2 = Color((255,255,102))
+
+colors = [c1, c2]
+exp = Color((204,179,179))
+
+#first select the two colors closest to the expected
+distances = []
 
 for c in colors:
-    red += math.pow(255-c.red,2)
-    green += math.pow(255-c.green,2)
-    blue += math.pow(255-c.blue,2)
-    
-red = round(255-math.sqrt(red)/2)
-green = round(255-math.sqrt(green)/2)
-blue = round(255-math.sqrt(blue)/2)
+    distances.append(distance(c.red,c.green,c.blue,exp.red,exp.green,exp.blue))
 
-result = Color((red,green,blue))
+#calculate all the different proportions for mixing the selected colors
+#store them in a dictionary "results"
+results = {}
+k = 10
+count = 0
+while count < k:
+    p = random.rand()
+    c = mix(c1.red, c1.green, c1.blue, c2.red, c2.green, c2.blue, p)
+    print(c)
+    results[p] = distance(exp.red,exp.green,exp.blue,c.red,c.green,c.blue)
+    count += 1
 
-#FITNESS
-#difference -> euclidean distance :: select mix with lowest distance
-distance = math.sqrt((math.pow(result.red-expected.red,2) + math.pow(result.green-expected.green,2) + math.pow(result.blue-expected.blue,2)))
+c = mix(c1.red, c1.green, c1.blue, c2.red, c2.green, c2.blue, 0.5)
+print(c)
+print (distance(exp.red,exp.green,exp.blue,c.red,c.green,c.blue))
+print(results)
 
-#manual distance
-##diffRed = abs(result.red - expected.red)
-##diffGreen = abs(result.green - expected.green)
-##diffBlue = abs(result.blue - expected.blue)
-##pctDiffRed = float(diffRed)/255
-##pctDiffGreen = float(diffGreen)/255
-##pctDiffBlue = float(diffBlue)/255
-##diff = ((pctDiffRed + pctDiffGreen + pctDiffBlue) / 3) * 100
+#use max with fitness, min with distance
+max_p = max(results, key=results.get)
+print(max_p)
+
+#sort dictionary by values descending order
+sorted(results.items(), key=lambda x: x[1], reverse=True)
+
 
 
 ##Gene: color
